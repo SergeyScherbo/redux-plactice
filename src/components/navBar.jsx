64967fb5b204b0f7e9dafe90a32a50c42ac5ffd6
Scherbo <sergey31scherbo@gmail.com>
2018-11-06 23:44:12 +0400

    navbar component

---
+++ src/components/navBar.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Link, NavLink, withRouter } from 'react-router-dom';
+
+const NavBar = (props) => {
+	setTimeout(() => {
+		props.history.push('/about');
+	}, 2000)
+	return (
+		<nav className="navigation">
+			<div className="container mod--alignCenter mod--spaceBetween">
+				<Link to="/" className="logo">Sample Logo</Link>
+				<ul>
+					<li><NavLink exact to="/">Home</NavLink></li>
+					<li><NavLink to="/about">About</NavLink></li>
+					<li><NavLink to="/contact">Contact</NavLink></li>
+				</ul>
+			</div>
+		</nav>
+	);
+}
+
+export default withRouter(NavBar);
