86a8daa2d6022671baea31945444d95a83ae8187
Scherbo <sergey31scherbo@gmail.com>
2018-11-06 23:43:59 +0400

    root component changes and routing

--- src/App.js
+++ src/App.js
@@ -1,36 +1,23 @@
 import React, { Component } from 'react';
-import Person from './components/person';
-import AddPerson from './components/addPerson';
+import { Route } from 'react-router-dom';
+import NavBar from './components/navBar';
+import Home from './routes/home';
+import About from './routes/about';
+import Contact from './routes/contact';
 
 class App extends Component {
   state = {
-    people: [
-      { name: 'Sergey', age: 24, gender: 'male' },
-      { name: 'Alice', age: 22, gender: 'female' },
-      { name: 'Diana', age: 18, gender: 'female' }
-    ]
-  }
 
-  handleAddPerson = person => e => {
-    e.preventDefault();
-    this.setState(state => ({ people: [...state.people, person] }));
-  }
-
-  handleRemovePerson = name => e => {
-    this.setState(state => ({ people: state.people.filter(person => person.name !== name) }));
   }
 
   render() {
     return (
       <div className="App">
-        <h1>My fifth React app!</h1>
-        <p>Hey there :)</p>
-        {
-          this.state.people.map(person => {
-            return <Person key={person.name} name={person.name} age={person.age} gender={person.gender} onRemovePerson={this.handleRemovePerson} />
-          })
-        }
-        <AddPerson onAddPerson={this.handleAddPerson} />
+        <NavBar />
+
+        <Route exact path="/" component={Home} />
+        <Route path="/about" component={About} />
+        <Route path="/contact" component={Contact} />
       </div>
     );
   }
