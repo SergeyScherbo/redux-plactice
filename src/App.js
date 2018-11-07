47f9fc0d7a16d82bc3330680edc215b92276a8a5
Scherbo <sergey31scherbo@gmail.com>
2018-11-07 23:49:58 +0400

    add switch add post components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
-import { Route } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 import NavBar from './components/navBar';
 import Home from './routes/home';
 import About from './routes/about';
 import Contact from './routes/contact';
+import Post from './routes/post';
 
 class App extends Component {
   state = {
@@ -14,10 +15,12 @@ class App extends Component {
     return (
       <div className="App">
         <NavBar />
-
-        <Route exact path="/" component={Home} />
-        <Route path="/about" component={About} />
-        <Route path="/contact" component={Contact} />
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/about" component={About} />
+          <Route path="/contact" component={Contact} />
+          <Route path="/:post_id" component={Post} />
+        </Switch>
       </div>
     );
   }
